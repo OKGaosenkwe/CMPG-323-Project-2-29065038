38d9d7928ab81a400dd17c4d2d2038cd579b472a
OfentseG <92356409+OKGaosenkwe@users.noreply.github.com>
2023-08-31 15:58:20 +0000

    Add files via upload

---
+++ Authentication/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using EcoPowerAPI.Models;
+using System.Reflection.Emit;
+
+namespace JWTAuthentication.Authentication
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customer { get; set; }
+        public virtual DbSet<Order> Order { get; set; }
+        public virtual DbSet<Product> Product { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+           // base.OnModelCreating(modelBuilder);
+            builder.Entity<OrderDetail>()
+            .HasKey(od => od.OrderDetailsId);
+
+            //modelBuilder.Entity<OrderDetail>().HasNoKey();
+            // entity.Property(e => e.OrderId).HasMaxLength(50);
+        }
+    }
+}
+
